87a305101c9459b00dc9c0d9d77b1e9a355efa9d
Githubmsr <sathireddyeee320@gmail.com>
2024-05-29 16:35:17 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
 import pandas as pd
@@ -9,6 +10,7 @@ st.write(
     """Choose the fruits you want in your custom smooothie!
     """
 )
+
 
 name_on_order = st.text_input("Name on smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
@@ -46,11 +48,20 @@ if ingredients_list:
  #st.write(my_insert_stmt)
  #st.stop()
 
+
 time_to_insert = st.button("Submit Order")
 if time_to_insert:
   session.sql(my_insert_stmt).collect()
   st.success('Your Smoothie is ordered MellyMe!', icon="✅")
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())    
-fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
+
+
+
+
+
+
+
+
+
+
+    
